c7feec608eec45e37d1b98723862864e05b5f455
interdimensionalpizza <michaellake05@gmail.com>
2023-06-07 16:33:01 -0700

    finished challenge

--- js/romanNumerals.js
+++ js/romanNumerals.js
@@ -1,3 +1,29 @@
-exports.toRoman = function(num) {
+let romanMatrix = [
+    [1000, 'M'],
+    [900, 'CM'],
+    [500, 'D'],
+    [400, 'CD'],
+    [100, 'C'],
+    [90, 'XC'],
+    [50, 'L'],
+    [40, 'XL'],
+    [10, 'X'],
+    [9, 'IX'],
+    [5, 'V'],
+    [4, 'IV'],
+    [1, 'I']
+  ];
+  
+  function convertToRoman(num) {
+    let answer = ''
+    for (let i = 0; i < romanMatrix.length; i++) {
+      if (num >= romanMatrix[i][0]) {
+            answer += romanMatrix[i][1];
+            num -= romanMatrix[i][0]
+            i--
+      }
+    }
+    return answer
+  }
 
-};
+console.log(convertToRoman(123))
